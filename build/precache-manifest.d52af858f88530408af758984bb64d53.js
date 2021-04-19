9878460a942c0f82364c8bef79917c04efc73734
AISHA-PRO <aisha1rashed@gmail.com>
2021-04-20 02:06:47 +0300

    Deployee

---
+++ build/precache-manifest.d52af858f88530408af758984bb64d53.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878de6a94498f2d1f328eb4dbb68be0f",
+    "url": "/IBTIKAR/index.html"
+  },
+  {
+    "revision": "dec629009f43160121f3",
+    "url": "/IBTIKAR/static/css/2.6ba1d82b.chunk.css"
+  },
+  {
+    "revision": "a8f5c015a08e2f2a4c0a",
+    "url": "/IBTIKAR/static/css/main.01824e4a.chunk.css"
+  },
+  {
+    "revision": "dec629009f43160121f3",
+    "url": "/IBTIKAR/static/js/2.2b16911f.chunk.js"
+  },
+  {
+    "revision": "6951e5822a376f3683b989464a621d5c",
+    "url": "/IBTIKAR/static/js/2.2b16911f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8f5c015a08e2f2a4c0a",
+    "url": "/IBTIKAR/static/js/main.ed425b51.chunk.js"
+  },
+  {
+    "revision": "971d3fe7fd77e77881d5",
+    "url": "/IBTIKAR/static/js/runtime-main.70099961.js"
+  },
+  {
+    "revision": "b01989e3c27e7c48bfc6113ae35a7a88",
+    "url": "/IBTIKAR/static/media/02.b01989e3.svg"
+  },
+  {
+    "revision": "1910389b1a7f5e425ed575ddde50db20",
+    "url": "/IBTIKAR/static/media/Ambar.1910389b.jpg"
+  },
+  {
+    "revision": "c0cecfe45e23e693d0680a93c5d07758",
+    "url": "/IBTIKAR/static/media/Apply.c0cecfe4.svg"
+  },
+  {
+    "revision": "745c8cf34cd13756196432bf5c615e4c",
+    "url": "/IBTIKAR/static/media/Arrow.745c8cf3.svg"
+  },
+  {
+    "revision": "3d07dcc9a40badb0e4a1426c4cfe5c65",
+    "url": "/IBTIKAR/static/media/ArrowLeft.3d07dcc9.svg"
+  },
+  {
+    "revision": "d03404099efbe8b512383fe4cf70630b",
+    "url": "/IBTIKAR/static/media/Atlas-Stripe.d0340409.svg"
+  },
+  {
+    "revision": "10ed481fb6b2b65f76ceed8f6a8be7d7",
+    "url": "/IBTIKAR/static/media/Bitmap.10ed481f.svg"
+  },
+  {
+    "revision": "5ab1d3ddb5001165fb8bfdae53e892a3",
+    "url": "/IBTIKAR/static/media/Facebook-Start.5ab1d3dd.svg"
+  },
+  {
+    "revision": "9917fd7fdda7c75d6f66d0e91eb5cb65",
+    "url": "/IBTIKAR/static/media/Gamephy.9917fd7f.svg"
+  },
+  {
+    "revision": "b7507b52171b777c1608893fd8e8b2ee",
+    "url": "/IBTIKAR/static/media/Gaza-Sky-Geeks.b7507b52.svg"
+  },
+  {
+    "revision": "23902d582fc3b5d8b1c72fc65ca69cf3",
+    "url": "/IBTIKAR/static/media/Group-6.23902d58.svg"
+  },
+  {
+    "revision": "a6fdb2a112538a4cdff321d20cfd8ecb",
+    "url": "/IBTIKAR/static/media/Hubspot-for-Startups.a6fdb2a1.svg"
+  },
+  {
+    "revision": "1dce4ca29da18df2e5f0d03642654d2b",
+    "url": "/IBTIKAR/static/media/Ibtikar-Board.1dce4ca2.svg"
+  },
+  {
+    "revision": "a08cb29621a86d0f76163367ad7093f7",
+    "url": "/IBTIKAR/static/media/Investment-1.a08cb296.svg"
+  },
+  {
+    "revision": "d4c4d5c868235d71f38069ec988ab45d",
+    "url": "/IBTIKAR/static/media/Investment-2.d4c4d5c8.svg"
+  },
+  {
+    "revision": "3d5aacf1f2282f88e7e552ade530f2a4",
+    "url": "/IBTIKAR/static/media/Investment-3.3d5aacf1.svg"
+  },
+  {
+    "revision": "fbd6b425bb0b0d33598903b8f168b343",
+    "url": "/IBTIKAR/static/media/Leaves.fbd6b425.svg"
+  },
+  {
+    "revision": "86dfb09acc771e430bc14919803c577f",
+    "url": "/IBTIKAR/static/media/Main Header.86dfb09a.svg"
+  },
+  {
+    "revision": "961786783e4b79fbc69c730ff4c8a682",
+    "url": "/IBTIKAR/static/media/Mashvisor.96178678.svg"
+  },
+  {
+    "revision": "d0b68667c727a55c89bec570303bcb04",
+    "url": "/IBTIKAR/static/media/Mr-.-Habib-Hazzan-ibtikar-fund.d0b68667.svg"
+  },
+  {
+    "revision": "228691e6ae243c3ad15e4f4d29c2abed",
+    "url": "/IBTIKAR/static/media/Mrs.-Ambar-Amleh-ibtikar-fund.228691e6.svg"
+  },
+  {
+    "revision": "5dcab8cb891ca1eff31f0b6e0af229b9",
+    "url": "/IBTIKAR/static/media/Portfolio 1.5dcab8cb.svg"
+  },
+  {
+    "revision": "bdefc488fa451c49cad03471e99fa633",
+    "url": "/IBTIKAR/static/media/RedCrow.bdefc488.svg"
+  },
+  {
+    "revision": "30b70b0edc158321cd3b33fa9997efc1",
+    "url": "/IBTIKAR/static/media/Reem-Qawasmi-ibtikar-fund.30b70b0e.svg"
+  },
+  {
+    "revision": "d5669170f18af3e656d1aacebdc1c1ec",
+    "url": "/IBTIKAR/static/media/Reem-Qawasmi-ibtikar-fund1.d5669170.svg"
+  },
+  {
+    "revision": "f4fadc50d0f259484a86af030fbd322f",
+    "url": "/IBTIKAR/static/media/Safrah.f4fadc50.svg"
+  },
+  {
+    "revision": "15f54cbadd5dca82e03dcd539dde3f1c",
+    "url": "/IBTIKAR/static/media/Seeds.15f54cba.svg"
+  },
+  {
+    "revision": "3dce0b5a0c167f022222adac97943dbb",
+    "url": "/IBTIKAR/static/media/TaawonLogo.3dce0b5a.svg"
+  },
+  {
+    "revision": "24d2ac0132fe99e38ee109791163f2e4",
+    "url": "/IBTIKAR/static/media/Untitled-design-1.24d2ac01.png"
+  },
+  {
+    "revision": "6e63d682d7b3eae8ee5253a4c0290fe2",
+    "url": "/IBTIKAR/static/media/Untitled-design-2.6e63d682.svg"
+  },
+  {
+    "revision": "7d8e754654ebc9802da5dc961fa649ea",
+    "url": "/IBTIKAR/static/media/Zahi-Khouri-IbtikarFund.7d8e7546.svg"
+  },
+  {
+    "revision": "05c940fedaef9bca8e9f7f148716aacd",
+    "url": "/IBTIKAR/static/media/bambinotes.05c940fe.svg"
+  },
+  {
+    "revision": "597c5d8323febac016f8372acb96ad47",
+    "url": "/IBTIKAR/static/media/ecal.597c5d83.svg"
+  },
+  {
+    "revision": "5e270697debd77570e854b628c8df7aa",
+    "url": "/IBTIKAR/static/media/fadfid.5e270697.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/IBTIKAR/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/IBTIKAR/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/IBTIKAR/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/IBTIKAR/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/IBTIKAR/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "16355ab90f3d083a17a353a1555bb24e",
+    "url": "/IBTIKAR/static/media/ibtikar-fund-1.16355ab9.svg"
+  },
+  {
+    "revision": "15472c5136693650d16eb214ee68bae7",
+    "url": "/IBTIKAR/static/media/icon_1.15472c51.svg"
+  },
+  {
+    "revision": "919605ac5881969aa2f18f29359bb0f2",
+    "url": "/IBTIKAR/static/media/icon_12.919605ac.svg"
+  },
+  {
+    "revision": "41b97d4e7f117fa1fc150ea02d9b1ba4",
+    "url": "/IBTIKAR/static/media/icon_13.41b97d4e.svg"
+  },
+  {
+    "revision": "f217e1fa1c56d2557615f9dae2b51dff",
+    "url": "/IBTIKAR/static/media/icon_14.f217e1fa.svg"
+  },
+  {
+    "revision": "69533dd240fe05678a4002623276451a",
+    "url": "/IBTIKAR/static/media/icon_5.69533dd2.svg"
+  },
+  {
+    "revision": "e57fc44eb188ec2395eb8c8888755b42",
+    "url": "/IBTIKAR/static/media/icon_7.e57fc44e.svg"
+  },
+  {
+    "revision": "24369bb216c1f822405da02ef3df55e3",
+    "url": "/IBTIKAR/static/media/icon_8.24369bb2.svg"
+  },
+  {
+    "revision": "8d351df822bbb7fc4500bd258b7569ca",
+    "url": "/IBTIKAR/static/media/icon_9.8d351df8.svg"
+  },
+  {
+    "revision": "cd1cc7f7b6625dc043544612cd6986d4",
+    "url": "/IBTIKAR/static/media/ingeez.cd1cc7f7.svg"
+  },
+  {
+    "revision": "7ffd4bad77b68e5f75815272a52a7801",
+    "url": "/IBTIKAR/static/media/kanz.7ffd4bad.svg"
+  },
+  {
+    "revision": "89a8efdc55b11b872c2c037ae6e9e290",
+    "url": "/IBTIKAR/static/media/leaders.89a8efdc.svg"
+  },
+  {
+    "revision": "ce71d2d5d3dcf4180c232770cb5f50cf",
+    "url": "/IBTIKAR/static/media/logo.ce71d2d5.png"
+  },
+  {
+    "revision": "af3a14807cf9571b00086e889559d153",
+    "url": "/IBTIKAR/static/media/play3rabi.af3a1480.svg"
+  },
+  {
+    "revision": "630585fa51f6b62f41e4e13d6a0dc20f",
+    "url": "/IBTIKAR/static/media/portfolio2.630585fa.svg"
+  },
+  {
+    "revision": "11bbf625fd58db632af519c5b834b44f",
+    "url": "/IBTIKAR/static/media/receet.11bbf625.svg"
+  },
+  {
+    "revision": "7f3af7b52aae5203056a52f4a02329ec",
+    "url": "/IBTIKAR/static/media/rokab.7f3af7b5.svg"
+  },
+  {
+    "revision": "caccc90300346df1392e52dcb3464461",
+    "url": "/IBTIKAR/static/media/social dice.caccc903.svg"
+  },
+  {
+    "revision": "416fc764a94c2165571d0a5312c5cde7",
+    "url": "/IBTIKAR/static/media/wirze.416fc764.svg"
+  }
+]);
